2b90b07df112c7c9954deff2dbc239c2c8a4a5b8
Sveta Vansovich <yansovich.ma@gmail.com>
2024-10-25 16:34:08 +0300

    e

--- script.js
+++ script.js
@@ -3,32 +3,54 @@ const fieldGame = document.getElementById('fieldGame');
 const enterBtn = document.getElementById('enterBtn');
 const clearBtn = document.getElementById('clearBtn');
 const backspaceBtn = document.getElementById('backspaceBtn');
+const buttons = document.querySelectorAll('.buttons button[data-value]');
+const score = document.querySelector('.score-value')
 const wave = document.getElementById('wave');
 let drops = [];
 let correctAnswers = 0;
 let wrongAnswers = 0;
-let interval = 5000; // начальное время в миллисекундах
+let interval = 4500; // начальное время в миллисекундах
 let animationDuration = 50000; // начальная продолжительность анимации в миллисекундах
+let currentScore = 0;
+let bonusInterval = 4000; // 10 секунд
+let bonusProbability = 0.1; // вероятность добавить класс bonus
 
-document.querySelectorAll('.buttons button[data-value]').forEach(button => {
+buttons.forEach(button => {
     button.addEventListener('click', () => {
         inputValue.value += button.dataset.value;
     });
 });
 
-enterBtn.addEventListener('click', () => {
+function checkResult() {
     const answer = parseInt(inputValue.value);
     inputValue.value = '';
 
     const dropIndex = drops.findIndex(drop => drop.answer === answer);
-    if (dropIndex !== -1) {
+
+    const hasBonusDrop = drops.some(drop => drop.answer === answer && drop.element.classList.contains('bonus'));
+
+    if (hasBonusDrop) {
+        correctAnswers = correctAnswers + 1;
+        increaseSpeed()
+        increaseScore()
+        console.log('BONUS!', correctAnswers);
+
+        drops.forEach(drop => {
+            drop.element.remove();
+        });
+        drops = [];
+        console.log('Все элементы drop удалены из-за бонуса');
+    }
+
+    else if (dropIndex !== -1) {
         drops[dropIndex].element.remove();
         drops.splice(dropIndex, 1);
-        correctAnswers++;
+        correctAnswers = correctAnswers + 1;
         // console.log('Правильный ответ:', answer);
         console.log(correctAnswers);
         increaseSpeed()
         console.log(interval);
+        increaseScore()
     } else {
         wrongAnswers = wrongAnswers + 1;
         wave.style.height = `${wave.clientHeight + 20}px`;
@@ -37,20 +59,75 @@ enterBtn.addEventListener('click', () => {
         if (wrongAnswers === 3) {
             alert('game over')
         }
+        reduceScore()
+    }
+}
+
+function clearInputValue() {
+    inputValue.value = ''
+}
+
+function deleteLastSymbol() {
+    inputValue.value = inputValue.value.slice(0, -1);
+}
+
+clearBtn.addEventListener('click', clearInputValue)
+backspaceBtn.addEventListener('click', deleteLastSymbol)
+enterBtn.addEventListener('click', checkResult)
+
+// клавиатура
+document.addEventListener('keydown', (event) => {
+    if (event.key.length === 1) {
+        inputValue.value += event.key
+    } else if (event.key === 'Backspace') {
+        deleteLastSymbol()
+    } else if (event.key === 'Enter') {
+        checkResult()
+    } else if (event.key === 'Escape') {
+        clearInputValue()
     }
 });
 
-clearBtn.addEventListener('click', () => {
-    inputValue.value = '';
-});
+// счет 
+function increaseScore() {
 
-backspaceBtn.addEventListener('click', () => {
-    inputValue.value = inputValue.value.slice(0, -1);
-});
+    // let previousScore = 0
+
+    if (correctAnswers === 1) {
+        let previousScore = 10;
+        score.textContent = previousScore;
+    } else {
+        currentScore = previousScore + 1 + 10;
+    }
+    score.textContent = currentScore;
+    console.log(currentScore);
+}
+
+function reduceScore() {
+    if (wrongAnswers > 0) { // Обработка неправильного ответа
+        if (wrongAnswers === 1) {
+            currentScore = currentScore - 5; // Первое неправильное
+        } else {
+            currentScore = currentScore - 6; // Последующие
+        }
+    }
+    if (currentScore < 0) {
+        currentScore = 0;
+    }
+    score.textContent = currentScore;
+    console.log(currentScore);
+}
+
 
 function createDrop() {
     const drop = document.createElement('div');
     drop.className = 'drop';
+
+    if (Math.random() < bonusProbability) {
+        drop.classList.add('bonus');
+    }
+    setInterval(() => { }, bonusInterval);
+
     const firstNumber = Math.floor(Math.random() * 10) + 1;
     let secondNumber;
 
@@ -71,6 +148,8 @@ function createDrop() {
     animateDrop(drop);
 }
 
+
+
 function animateDrop(drop) {
     drop.animate([{ transform: 'translateY(0)' }, { transform: 'translateY(calc(100vh - 150px))' }], {
         duration: animationDuration,
@@ -87,7 +166,7 @@ function animateDrop(drop) {
         if (wave.style.height === '650px') {
             alert('game over')
         }
-    }, 500)   
+    }, 500)
 }
 
 function isDropInWave(drop) {
@@ -97,15 +176,16 @@ function isDropInWave(drop) {
 }
 
 // увеличить скорость 
-function increaseSpeed () {
+function increaseSpeed() {
     console.log(correctAnswers, 1000000000000);
     const settings = {
-        10: { interval: 4500, animationDuration: 45000 },
-        20: { interval: 4000, animationDuration: 40000 },
-        30: { interval: 3500, animationDuration: 35000 },
-        40: { interval: 3000, animationDuration: 30000 },
-        50: { interval: 2500, animationDuration: 25000 },
-        60: { interval: 2000, animationDuration: 20000 },
+        10: { interval: 4000, animationDuration: 45000 },
+        20: { interval: 3500, animationDuration: 40000 },
+        30: { interval: 3000, animationDuration: 35000 },
+        40: { interval: 2500, animationDuration: 30000 },
+        50: { interval: 2000, animationDuration: 25000 },
+        60: { interval: 1500, animationDuration: 20000 },
+        70: { interval: 1000, animationDuration: 15000 },
     };
 
     if (settings[correctAnswers]) {
